806c1aaf2f5c6d684aeda710c6e1c358495d600b
Ramon Lansangan <richersoon04@gmail.com>
2015-05-13 23:33:28 +0800

    before refractoring
    Signed-off-by: Ramon Lansangan <richersoon04@gmail.com>

--- LingoProject/src/test/java/com/padistutorial/LingoProject/LingoGameTest.java
+++ LingoProject/src/test/java/com/padistutorial/LingoProject/LingoGameTest.java
@@ -23,4 +23,50 @@ public class LingoGameTest {
 		LingoGame lingoGame = (LingoGame) ctx.getBean("myLingoGame");
 		lingoGame.startGame();
 	}
+	
+	//we are going to test the entire system of lingo game
+	@Test
+	public void testLingoGame() {
+		//instantiate the lingoGame with the spring 
+		LingoGame lingoGame = (LingoGame) ctx.getBean("myLingoGame");
+		
+		//display "the game has started"
+		lingoGame.startGame();
+		
+		//we are going to use mockito in order to provide a fake set of words in which the playable one
+		//will be choosen
+		lingoGame.setWordsFromDataSource(getWordsFromPackedDataSource());
+		
+		//selects a word between all the words of the faked Datasource
+		lingoGame.selectAWordFromTheFakedDataSource();
+		
+		//displays some of the letters of the selected word in the right order
+		//i.e for the word "TUTORIAL", the display could be "T.T....."
+		lingoGame.initLayout();
+		
+		//looping for the number of chances of the player
+		for(int i=0; i<lingoGame.getNumberOfChanges();i++) {
+			//wait for the user to seize his answer
+			lingoGame.waitForAnswer();
+			
+			//checks whether the answer has correctly been seized
+			//if a word is well located, it will appear in capital letter
+			//if not, it will be in small letter 
+			if(lingoGame.checkAnswer()) {
+				
+				//display "the game has been won"
+				lingoGame.theGameIsWon();
+				
+				//reintantiates all the variables
+				lingoGame.stopGame();
+			}
+		}
+		
+
+		//display "the game has been lost"
+		lingoGame.theGameIsLost();
+		
+		//reintantiates all the variables
+		lingoGame.stopGame();
+	}
 }
